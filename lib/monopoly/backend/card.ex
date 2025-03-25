88ff85f2db1d4de263e39b262237751c80bf6e8a
NATHAN YAU <nshyau@gmail.com>
2025-03-24 21:44:04 -0700

    Add card module

---
+++ lib/monopoly/backend/card.ex
@@ -0,0 +1,31 @@
+defmodule GameObjects.Card do
+  @moduledoc """
+  This module represents a card and their attributes.
+  """
+
+  defstruct [:id, :name, :type, :effect, :owned]
+
+  def apply_effect(%__MODULE__{effect: {:pay, amount}}, player) do
+    %{player | money: player.money - amount}
+  end
+
+  def apply_effect(%__MODULE__{effect: {:earn, amount}}, player) do
+    %{player | money: player.money + amount}
+  end
+
+  def apply_effect(%__MODULE__{effect: {:get_out_of_jail, true}, owned: true} = card, player) do
+    %{player |
+      get_out_of_jail: true,
+      card: Enum.reject(player.card, fn c -> c.id == card.id end)  # Remove from player's cards
+    }
+  end
+
+  def apply_effect(%__MODULE__{effect: {:get_out_of_jail, true}}, player) do
+    %{player | get_out_of_jail: true}
+  end
+
+  def apply_effect(_, player), do: player
+
+  # Marks the card as 'owned'
+  def mark_as_owned(card), do: %{card | owned: true}
+end
