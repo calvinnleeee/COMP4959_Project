86172e5a0a9451a064beae1f7b4612e6f365599e
JoanneH2050 <122232283+JoanneH2050@users.noreply.github.com>
2025-03-24 22:41:47 -0700

    add dice and jail logic

--- lib/monopoly/backend/dice.ex
+++ lib/monopoly/backend/dice.ex
@@ -5,7 +5,18 @@ defmodule GameObjects.Dice do
 
   # Roll and return a tuple of two random integers [1-6]
   def roll() do
-    # return
+    die1 = :rand.uniform(6)
+    die2 = :rand.uniform(6)
+    {die1, die2}
+  end
+
+  # Determine the outcome of a jail roll based on the dice values and the number of attempts
+  def jail_roll({d1, d2}, attempt_num) do
+    cond do
+      d1 == d2 -> {:out_of_jail, {d1, d2}}
+      attempt_num >= 3 -> {:failed_to_escape, {d1, d2}}
+      true -> {:stay_in_jail, {d1, d2}}
+    end
   end
 
 end
