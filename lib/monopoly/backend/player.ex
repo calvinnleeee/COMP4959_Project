64fd7315ecd1f1fe4d7cf4c63aad3b4830d197e0
matthewy0987 <myoon928@gmail.com>
2025-03-29 01:47:40 -0700

    create getters and settes for players

--- lib/monopoly/backend/player.ex
+++ lib/monopoly/backend/player.ex
@@ -7,6 +7,42 @@ defmodule GameObjects.Player do
   id is the session id of the player.
   """
 
-  defstruct [:id, :name, :money, :sprite_id, :position, :cards, :in_jail, :jail_turns]
+    defstruct [:id, :name, :money, :sprite_id, :position, :cards, :in_jail, :jail_turns]
+
+    def get_id(player) do
+        player.id
+    end
+
+    def get_money(player) do
+        player.money
+    end
+
+    def get_position(player) do
+        player.position
+    end
+
+    def get_in_jail(player) do
+        player.in_jail
+    end
+
+    def set_money(player, num) do
+        %{player | money: get_money(player) + num}
+    end
+
+    def set_jail_turn(player, num) do
+        %{player | jail_turns: num}
+    end
+
+    def add_card(player, card) do
+        %{player | cards: [card | get_cards(player)] }
+    end
+
+    def remove_card(player, card) do
+        %{player | cards: List.delete(get_cards(player), card) }
+    end
+
+    def lose_money(player, amount) do
+        %{player | money: get_money(player) - amount }
+    end
 
 end
