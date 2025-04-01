46fa24b324847de21741fd17a60902a2d50ad971
Caleb Chiang <calebchiang1002@gmail.com>
2025-04-01 10:35:03 -0700

    Set game_lobby as root page

--- lib/monopoly_web/router.ex
+++ lib/monopoly_web/router.ex
@@ -19,8 +19,6 @@ defmodule MonopolyWeb.Router do
 
     live "/", WelcomeLive, :index
 
-    live "/game-dummy", GameDummy, :index # dummy screen to redirect to after "Start Game" button is clicked
-
     live "/game", GameLive, :index
     live "/game/:id", GameLive, :show
 
