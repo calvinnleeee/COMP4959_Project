47cc1fdc40be47dfcd8268ebeb0a909a190d6fdb
Caleb Chiang <calebchiang1002@gmail.com>
2025-04-01 10:35:03 -0700

    Set game_lobby as root page

--- lib/monopoly_web/router.ex
+++ lib/monopoly_web/router.ex
@@ -17,7 +17,7 @@ defmodule MonopolyWeb.Router do
   scope "/", MonopolyWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    get "/", PageController, :game_lobby
 
     live "/game", GameLive, :index
     live "/game/:id", GameLive, :show
