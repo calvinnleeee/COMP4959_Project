defe80b2dfb646baa2338a791cbf3d2623e02ba7
Inez Yoon <inez3cs@gmail.com>
2025-04-02 15:30:44 -0700

    Add create player with default value

--- test/game_objects/player_test.exs
+++ test/game_objects/player_test.exs
@@ -3,6 +3,10 @@ defmodule GameObjects.PlayerTest do
   use ExUnit.Case
   alias GameObjects.Player
 
+  # Global Variable
+  # @initial_money 1500
+  # @board_size 40
+
   # Hardcoded test
   @player_id "albert123"
   @player_name "Albert"
@@ -12,6 +16,21 @@ defmodule GameObjects.PlayerTest do
   setup do
     player = Player.new(@player_id, @player_name, @sprite_id)
     %{player: player}
+  end
+
+  # Create player with default value : ok
+  test "new/3 creates a player with default values", %{player: player} do
+    assert player.id == @player_id
+    assert player.name == @player_name
+    assert player.money == 1500
+    assert player.sprite_id == @sprite_id
+    assert player.position == 0
+    assert player.properties == []
+    assert player.cards == []
+    assert player.in_jail # Boolean
+    assert player.jail_turns == 0
+    assert player.turns_take == 0
+    assert player.rolled # Boolean
   end
 
 end
