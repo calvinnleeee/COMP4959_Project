e3c0cb4c9189b9d7dfe33fb435ed2665aeeff0eb
Inez Yoon <inez3cs@gmail.com>
2025-04-02 15:52:46 -0700

    Set money

--- test/game_objects/player_test.exs
+++ test/game_objects/player_test.exs
@@ -10,20 +10,20 @@ defmodule GameObjects.PlayerTest do
   # Hardcoded test
   @player_id "albert123"
   @player_name "Albert"
-  @sprite_id 1
+  @player_sprite_id 1
 
   # Basic setup test for hardcoded test : OK
   setup do
-    player = Player.new(@player_id, @player_name, @sprite_id)
+    player = Player.new(@player_id, @player_name, @player_sprite_id)
     %{player: player}
   end
 
-  # new(id, name, sprite_id) creates player with default value : ok
+  # new(id, name, player_sprite_id) creates player with default value : ok
   test "new/3 creates a player with default values", %{player: player} do
     assert player.id == @player_id
     assert player.name == @player_name
     assert player.money == 1500
-    assert player.sprite_id == @sprite_id
+    assert player.player_sprite_id == @player_sprite_id
     assert player.position == 0
     assert player.properties == []
     assert player.cards == []
@@ -33,16 +33,28 @@ defmodule GameObjects.PlayerTest do
     assert player.rolled # Boolean
   end
 
-  # new(id, name, sprite_id) returns correct values : ERROR -> line 127 typo (player.jail_turn's')
-  test "Getters for return correct values", %{player: player} do
+  # new(id, name, player_sprite_id) returns correct values : ERROR -> line 127 typo (player.jail_turn's')
+  test "Getters for returning correct values", %{player: player} do
     assert Player.get_id(player) == @player_id
     assert Player.get_name(player) == @player_name
     assert Player.get_money(player) == 1500
-    assert Player.get_sprite_id(player) == @sprite_id
+    assert Player.get_sprite_id(player) == @player_sprite_id
     assert Player.get_position(player) == 0
     assert Player.get_properties(player) == []
     assert Player.get_cards(player) == []
     refute Player.get_in_jail(player)
     assert Player.get_jail_turns(player) == 0
   end
+
+  # M O N E Y
+  # set_money(__MODULE__.t(), integer()) :: __MODULE__.t(): OK
+  test "set_money/2 updates player's money", %{player: player} do
+    updated = Player.set_money(player, 100)
+    assert updated.money == 100
+  end
+  # add_money(__MODULE__.t(), integer()) :: __MODULE__.t()
+  # @spec lose_money(__MODULE__.t(), integer()) :: __MODULE__.t()
+  # @spec lose_money(__MODULE__.t(), __MODULE__.t(), integer()) :: {__MODULE__.t(), __MODULE__.t()}
+
+
 end
