7b8efbdfc12723b80c61487adf3b492714ada9dd
Caleb Chiang <calebchiang1002@gmail.com>
2025-04-03 18:55:30 -0700

    Updated to redirect to dummy page when game is started

--- lib/monopoly_web/live/welcome_live.ex
+++ lib/monopoly_web/live/welcome_live.ex
@@ -6,7 +6,7 @@ defmodule MonopolyWeb.WelcomeLive do
   # Initializes socket state when the LiveView mounts
   def mount(_, _, socket) do
     if connected?(socket), do: Phoenix.PubSub.subscribe(Monopoly.PubSub, "game_state")
-    {:ok, assign(socket, show_modal: false)}
+    {:ok, assign(socket, show_modal: false, game_started: false)}
   end
 
   # Handles event when "Join Game" is clicked
@@ -43,12 +43,28 @@ defmodule MonopolyWeb.WelcomeLive do
     current_player = Enum.find(state.players, fn p -> p.id == socket.assigns.session_id end)
     sprite_id = current_player && current_player.sprite_id || nil
 
-    {:noreply, assign(socket, players: state.players, sprite_id: sprite_id)}
+    game_started = state.current_player != nil
+
+    # If the game has started, push a navigation to redirect the user to the game screen.
+    # Otherwise, do nothing and keep the user on the current page.
+    socket =
+      if state.current_player != nil do
+        push_navigate(socket, to: "/game-dummy")
+      else
+        socket
+      end
+
+    {:noreply, assign(socket, players: state.players, sprite_id: sprite_id, game_started: game_started)}
   end
 
   # Handles real time update when game is deleted
   def handle_info(%{event: "game_deleted"}, socket) do
     {:noreply, assign(socket, players: [], show_modal: false)}
+  end
+
+  def handle_event("start_game", _, socket) do
+    Game.start_game()
+    {:noreply, socket}
   end
 
   # Renders the LiveView HTML, including the modal if show_modal is true
@@ -72,7 +88,7 @@ defmodule MonopolyWeb.WelcomeLive do
     </main>
 
     <%= if @show_modal do %>
-      <.lobby_modal players={@players} sprite_id={@sprite_id}  />
+      <.lobby_modal players={@players} sprite_id={@sprite_id} />
     <% end %>
 
     """
